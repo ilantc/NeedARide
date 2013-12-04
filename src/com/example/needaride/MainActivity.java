995e68c0e8939123662b05ce3a6e74291f787083
TomBlinder <tomblinder@gmail.com>
2013-12-04 21:03:30 +0200

    changed linear to relative in form_basic_ride.xml

--- src/com/example/needaride/MainActivity.java
+++ src/com/example/needaride/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.needaride;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button mapBT = (Button)findViewById(R.id.mapButton);
+		mapBT.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "mapButton", Toast.LENGTH_SHORT).show();
+
+				}
+		});
+		Button favouritesBT = (Button)findViewById(R.id.favouritesButton);
+		favouritesBT.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "favouritesButton", Toast.LENGTH_SHORT).show();
+				}
+		});
 	}
 
 	@Override
