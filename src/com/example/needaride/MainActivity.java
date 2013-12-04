584d555cac9d41c2614e3b16db83925c1d030bda
TomBlinder <tomblinder@gmail.com>
2013-12-04 21:43:09 +0200

    add buttons in the main activity

--- src/com/example/needaride/MainActivity.java
+++ src/com/example/needaride/MainActivity.java
@@ -2,11 +2,11 @@ package com.example.needaride;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -16,12 +16,34 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button offerRideBT = (Button) findViewById(R.id.offerRide);
+		ImageButton offerRideBT = (ImageButton) findViewById(R.id.offerRideBT);
 		offerRideBT.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {				
 				(new TaxiOrPrivateDialogGetter(v.getContext())).showDialog();
 			}
+		});
+		
+		ImageButton offerOrTakeARideBT = (ImageButton) findViewById(R.id.offerOrTakeARideBT);
+		offerRideBT.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {				
+				(new TaxiOrPrivateDialogGetter(v.getContext())).showDialog();
+			}
+		});
+		
+		Button favouritesBT = (Button) findViewById(R.id.favouritesBT);
+		favouritesBT.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {				
+				Toast.makeText(getApplicationContext(), "favourites button clicked", Toast.LENGTH_SHORT).show();
+			}
+		});
+		Button mapBT = (Button) findViewById(R.id.mapButtonBT);
+		mapBT.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {				
+				Toast.makeText(getApplicationContext(), "map button clicked", Toast.LENGTH_SHORT).show();}
 		});
 	}
 
