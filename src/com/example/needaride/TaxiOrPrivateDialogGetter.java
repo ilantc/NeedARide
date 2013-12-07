66fccba35ae0273edd51a3dc3f5dbdc13b59e0e6
TomBlinder <tomblinder@gmail.com>
2013-12-07 11:54:22 +0200

    dialog_private_or_taxi.xml icon changed

--- src/com/example/needaride/TaxiOrPrivateDialogGetter.java
+++ src/com/example/needaride/TaxiOrPrivateDialogGetter.java
@@ -3,6 +3,7 @@ package com.example.needaride;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import android.app.Dialog;
 import android.content.Context;
@@ -24,9 +25,8 @@ public class TaxiOrPrivateDialogGetter  {
 		dialog.setTitle(R.string.TaxiOrPrivateDialogHeader);
 		
 		// set the custom dialog buttons
-		Button taxiB 	= (Button) dialog.findViewById(R.id.taxiButtonDialog);
-		Button privateB = (Button) dialog.findViewById(R.id.privateButtonDialog);		
-		
+		ImageButton taxiB 	= (ImageButton) dialog.findViewById(R.id.taxiButtonDialog);
+		ImageButton privateB = (ImageButton) dialog.findViewById(R.id.privateButtonDialog);		
 		// if button is clicked, close the custom dialog
 		taxiB.setOnClickListener(new OnClickListener() {
 			@Override
@@ -53,6 +53,11 @@ public class TaxiOrPrivateDialogGetter  {
 		});
 		
 		dialog.show();
+	}
+
+	private void setTheme(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
