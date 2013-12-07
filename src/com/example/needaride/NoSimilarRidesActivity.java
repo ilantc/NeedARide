4d24a8cd5d8c3b9008905c0b24156cb5a1fb827a
TomBlinder <tomblinder@gmail.com>
2013-12-07 14:08:52 +0200

    NoSimilarRidesActivity.java changed

--- src/com/example/needaride/NoSimilarRidesActivity.java
+++ src/com/example/needaride/NoSimilarRidesActivity.java
@@ -2,7 +2,13 @@ package com.example.needaride;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class NoSimilarRidesActivity extends Activity {
 
@@ -10,8 +16,18 @@ public class NoSimilarRidesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_no_similar_rides);
-	}
+	
 
+	ImageButton openARideIMGBT = (ImageButton) findViewById(R.id.goHomeIMGBT);
+			openARideIMGBT.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {				
+					Intent intent = new Intent();
+					intent.setClassName(getPackageName(), getPackageName() +".MainActivity");
+					startActivity(intent);
+				}
+			});
+	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
