083d81daf79c1108e2f1a9cd5d0b0bd055f8d8bc
TomBlinder <tomblinder@gmail.com>
2013-12-18 16:20:10 +0200

    get location

--- src/com/example/needaride/MapActivity.java
+++ src/com/example/needaride/MapActivity.java
@@ -2,11 +2,16 @@ package com.example.needaride;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -19,7 +24,18 @@ public class MapActivity extends FragmentActivity  {
 		
 		if (ConnectionResult.SUCCESS == GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()) ){
 			Toast.makeText(getApplicationContext(), "maps are avaliable", Toast.LENGTH_LONG).show();
-			GoogleMap map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			GoogleMap mMap = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			mMap.setMyLocationEnabled(true);
+			Log.e("BUG", "111111111111111111111");
+			Location myLoc = mMap.getMyLocation();
+			Log.e("BUG", "222222222222222222222");
+			LatLng latLng = new LatLng(myLoc.getLatitude(), myLoc.getLongitude());
+			Log.e("BUG", "333333333333333333333");
+			CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
+			Log.e("BUG", "boooooooooooooooooooooooooooooooo");
+			mMap.animateCamera(cameraUpdate);   
+			//CameraUpdate cu = CameraUpdateFactory.(myLoc.getLatitude(),myLoc.getLongitude());
+			
 		}
 	}
 
