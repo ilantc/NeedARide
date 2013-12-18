9ca1f76a6231f10199cdbdf58b141f26c9eaa542
ilantc <ilantch@gmail.com>
2013-12-18 17:26:07 +0200

    adding location client for getting current location

--- src/com/example/needaride/MapActivity.java
+++ src/com/example/needaride/MapActivity.java
@@ -1,7 +1,9 @@
 package com.example.needaride;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,26 +17,24 @@ import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
-public class MapActivity extends FragmentActivity  {
-
+public class MapActivity extends FragmentActivity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
+	
+	LocationClient mLocationClient;
+	GoogleMap mMap;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		mLocationClient = new LocationClient(this, this, this);
+		
+		
 		
 		if (ConnectionResult.SUCCESS == GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()) ){
 			Toast.makeText(getApplicationContext(), "maps are avaliable", Toast.LENGTH_LONG).show();
-			GoogleMap mMap = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+			mMap = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
 			mMap.setMyLocationEnabled(true);
-			Log.e("BUG", "111111111111111111111");
-			Location myLoc = mMap.getMyLocation();
-			Log.e("BUG", "222222222222222222222");
-			LatLng latLng = new LatLng(myLoc.getLatitude(), myLoc.getLongitude());
-			Log.e("BUG", "333333333333333333333");
-			CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-			Log.e("BUG", "boooooooooooooooooooooooooooooooo");
-			mMap.animateCamera(cameraUpdate);   
-			//CameraUpdate cu = CameraUpdateFactory.(myLoc.getLatitude(),myLoc.getLongitude());
 			
 		}
 	}
@@ -45,5 +45,45 @@ public class MapActivity extends FragmentActivity  {
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
 	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		Log.e("BUG", "111111111111111111111");
+		Location myLoc = mLocationClient.getLastLocation();
+		Log.e("BUG", "222222222222222222222");
+		LatLng latLng = new LatLng(myLoc.getLatitude(), myLoc.getLongitude());
+		Log.e("BUG", "333333333333333333333");
+		CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 12);
+		Log.e("BUG", "boooooooooooooooooooooooooooooooo");
+		mMap.animateCamera(cameraUpdate);   
+		
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+    protected void onStart() {
+        super.onStart();
+        // Connect the client.
+        mLocationClient.connect();        
+    }
+
+    /*
+     * Called when the Activity is no longer visible.
+     */
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
+        mLocationClient.disconnect();
+        super.onStop();
+    }
+	
 
 }
